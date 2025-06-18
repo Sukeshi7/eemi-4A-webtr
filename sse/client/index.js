17ec784a6f5fd58011047ec47a28773736e5d739
Karl MARQUES <karl@kmarques.dev>
2025-06-18 15:23:21 +0200

    WIP SSE

---
+++ sse/client/index.js
@@ -0,0 +1,62 @@
+let username;
+
+document.getElementById("login").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const data = new FormData(e.currentTarget);
+  username = data.get("username");
+  document
+    .getElementById("username")
+    .appendChild(document.createTextNode(username));
+  const eventSource = new EventSource(
+    "http://localhost:3000/messages/subscribe"
+  );
+  eventSource.addEventListener("message", function (e) {
+    const data = JSON.parse(e.data);
+    switch (data.type) {
+      case "new-message":
+        addMessage(data.payload);
+        break;
+    }
+  });
+  document.getElementById("chat").style.display = "block";
+  e.currentTarget.style.display = "none";
+});
+
+document.getElementById("send").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const data = new FormData(e.currentTarget);
+  const message = data.get("message");
+  fetch("http://localhost:3000/messages", {
+    headers: {
+      "content-type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      message,
+    }),
+  })
+    .then((resp) => {
+      if (resp.ok) alert("Message sent");
+      else alert("Failed to send message");
+    })
+    .catch(() => alert("Failed to send message"));
+});
+
+function addMessage(message) {
+  const messageItem = document.createElement("div");
+
+  messageItem.style.alignSelf =
+    message.username === username ? "self-end" : "self-start";
+  messageItem.style.backgroundColor =
+    message.username === username ? "blue" : "white-smoke";
+  messageItem.style.color = message.username === username ? "white" : "black";
+
+  messageItem.appendChild(
+    document.createTextNode(
+      `${message.message}` +
+        (message.username !== username ? ` - ${message.username}` : "")
+    )
+  );
+  document.getElementById("messages").appendChild(messageItem);
+}
---
+++ sse/server/index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const messages = [];
+let subscribers = [];
+
+function addSubscriber(res) {
+  const subscriber = {
+    id: Date.now(),
+    res,
+  };
+  console.log(`Adding subscriber ${subscriber.id}`);
+  subscribers.push(subscriber);
+  res.on("close", () => removeSubscriber(subscriber.id));
+  res.on("error", () => removeSubscriber(subscriber.id));
+}
+
+function removeSubscriber(id) {
+  console.log(`Removing subscriber ${id}`);
+  const index = subscribers.findIndex((s) => s.id == id);
+  if (index !== -1) subscribers.splice(index, 1);
+}
+
+function clearSubscribers() {
+  subscribers.splice(0, Infinity);
+}
+
+app.post("/messages", (req, res) => {
+  const message = {
+    id: Date.now(),
+    ...req.body,
+  };
+  messages.push(message);
+  broadcast({
+    type: "new-message",
+    payload: message,
+  });
+  res.sendStatus(201);
+});
+
+app.get("/messages/subscribe", (req, res) => {
+  addSubscriber(res);
+  res.writeHead(200, {
+    connection: "keep-alive",
+    "content-type": "text/event-stream",
+    "cache-control": "no-cache",
+  });
+
+  res.write(": ping\n\n");
+  setInterval(function () {
+    res.write(": ping\n\n");
+  }, 10 * 1000);
+});
+
+function broadcast(event) {
+  console.log(`${subscribers.length} subscribers to notify`);
+  for (let { id, res } of subscribers) {
+    console.log(`Sending event to ${id}`);
+    res.write(`data: ${JSON.stringify(event)}`);
+    res.write(`\n\n`);
+  }
+}
+
+app.listen(3000, () => console.log("Server listening on port 3000"));
